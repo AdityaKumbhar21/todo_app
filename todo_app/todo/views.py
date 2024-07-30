8520bcf9685a3b071c290b7f5b5568e17748e012
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-07-30 11:49:22 +0530

    added home view nad add_todo feature

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def home(request):
-    return render(request, 'todo/home.html')
+    todos = Todo.objects.all()
+    return render(request, 'todo/home.html',{'todos':todos})
+
+@login_required
+def add_todo(request):
+    if request.method == "POST":
+        data = request.POST
+        task = data.get('task')
+        task_description = data.get('task_description')
+
+        Todo.objects.create(
+            task = task,
+            task_description =  task_description 
+        )
+        return redirect('home') 
+
+    return render(request,'todo/add_todo.html')
+
