5cc61c8c53ce8bcc7f7b45986e4d286ae221e3db
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-07-30 10:36:45 +0530

    created landing page and model

--- todo_app/todo/models.py
+++ todo_app/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length=100)
+    task_description  = models.TextField(max_length=250)
+    added_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
